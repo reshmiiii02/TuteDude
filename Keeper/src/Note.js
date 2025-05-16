f206ae01f73a3baa9d906714d5111b0bd3c78351
reshmiiii02 <reshmisuryavanshi785@gmail.com>
2025-05-16 15:25:01 +0530

    Add files via upload

---
+++ Keeper/src/Note.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles1.css";
+
+function Note(props){
+    function handleClick(){
+        props.onDelete(props.id)
+    }
+    return(
+        <div className='note'>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
+            <button className="delete-button" onClick={handleClick}>DELETE</button>
+        </div>
+    );
+}
+export default Note;
---
+++ Keeper/src/notes.js
@@ -0,0 +1,22 @@
+const notes = [
+  {
+    key: 1,
+    title: "Delegation",
+    content: "Q. How many programmers does it take to change a light bulb? A. None. That's a hardware problem."
+  },
+  {
+    key: 2,
+    title: "Loops",
+    content: "How to keep a programmer in the shower forever.\nA. Tell them to 'lather, rinse, repeat'."
+  },
+  {
+    key: 3,
+    title: "Arrays",
+    content: "Q. Why did the programmer quit his job? A. Because he didn't get arrays (a raise)."
+  },
+  {
+    key: 4,
+    title: "Hardware vs. Software",
+    content: "If you play with software long enough, it will break. If you play with hardware long enough, it will work."
+  }
+];
